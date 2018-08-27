383f24ccf367715057b944588634d7a829494a94
ekellener <erik@kellener.com>
2018-08-26 21:10:06 -0700

    removed typescript changes and added missing module

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const sis = require('./lib/sis');
 const course = require('./lib/course');
 const assignment = require('./lib/assignment');
 const cmodule = require('./lib/module');
+const quiz = require('./lib/quiz');
 
-let canvas = { helpers, endpoint, sis, course, assignment, cmodule };
+let canvas = { helpers, endpoint, sis, course, assignment, cmodule, quiz };
 
 module.exports = canvas;
 
